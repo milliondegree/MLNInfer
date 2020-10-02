277eafe9ad3836d44f07801db92a3aed3d96df95
milliondegree <zhangjiachi1007@gmail.com>
2020-10-02 16:01:02 -0400

    query.sh

---
+++ query.sh
@@ -0,0 +1,4 @@
+for v in 1 2 3 4 5 6 7 8 9 10
+do 
+  ./build/main -o data/hypertext-class/sample7/sample7$v.obs -p data/hypertext-class/sample7/prov/sample7$v.txt -q all > data/hypertext-class/sample7/records/sample$v.log
+done
