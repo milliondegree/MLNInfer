45c6baeebf6ab8a4078fbc7109c360e4021e8145
milliondegree <zhangjiachi1007@gmail.com>
2020-10-05 02:30:52 -0400

    avg times

---
+++ averageTime.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+
+def parseFile(f):
+  dic = {}
+  times = []
+  tuples = []
+  for line in f.readlines():
+    line = line.strip()
+    splits = line.split(" ")
+    if splits[0]=="pgibbs":
+      times.append(float(splits[3]))
+    elif splits[0]=="probability":
+      tuples.append(splits[2])
+  for t, time in zip(tuples, times):
+    dic[t] = time
+  return dic
+
+
+
+  def selectDict(dic, l):
+    ndic = {}
+    for key in dic:
+      if key in l:
+        ndic[key] = dic[key]
+    return ndic
+
+
+if __name__=="__main__":
+  file_names = [
+            "./data/hypertext-class/sample7/records/sample1.log",
+            "./data/hypertext-class/sample7/records/sample2.log",
+            "./data/hypertext-class/sample7/records/sample3.log",
+            "./data/hypertext-class/sample7/records/sample4.log",
+            "./data/hypertext-class/sample7/records/sample5.log",
+            "./data/hypertext-class/sample7/records/sample6.log",
+            "./data/hypertext-class/sample7/records/sample7.log",
+            "./data/hypertext-class/sample7/records/sample8.log",
+            "./data/hypertext-class/sample7/records/sample9.log",
+            "./data/hypertext-class/sample7/records/sample10.log"
+          ]
+  dics = []
+  avg_times = []
+  for file_name in file_names:
+    file = open(file_name, "r")
+    dic = parseFile(file)
+    dics.append(dic)
+    dics[i] = selectDict(dics[i], dics[0].keys)
+    avg_times.append(np.array(dics[i]).mean())
+  print(avg_times)
