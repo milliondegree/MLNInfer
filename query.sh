ede90bf20aa8009988bae8c37ceb0349d6dba584
milliondegree <zhangjiachi1007@gmail.com>
2020-10-11 19:56:47 -0400

    query gibbs

--- query.sh
+++ query.sh
@@ -1,4 +1,5 @@
 for v in 1 2 3 4 5 6 7 8 9 10
 do 
-  ./build/main -o data/hypertext-class/sample7/sample7$v.obs -p data/hypertext-class/sample7/prov/sample7$v.txt -q all -r 10000 > data/hypertext-class/sample7/records/Gibbssample$v.log
+  echo $v
+  ./build/main -o data/hypertext-class/sample7/sample7$v.obs -p data/hypertext-class/sample7/prov/sample7$v.txt -q all -r 10000 -m gibbs > data/hypertext-class/sample7/records/Gibbssample$v.log
 done
