9463432963756dcc9be79aef2c18be9ccb85e6ce
milliondegree <zhangjiachi1007@gmail.com>
2020-10-25 23:48:12 -0400

    renew average time

--- averageTime.py
+++ averageTime.py
@@ -1,68 +1,48 @@
 import numpy as np
 
 
-def parseFile(f):
+
+def parseFileFinal(f):
   dic = {}
-  times = []
-  tuples = []
-  for line in f.readlines():
-    line = line.strip()
-    splits = line.split(" ")
-    if splits[0]=="pgibbs":
-      times.append(float(splits[3]))
-    elif splits[0]=="probability":
-      tuples.append(splits[2])
-  for t, time in zip(tuples, times):
-    dic[t] = time
-  return dic
-
-
-
-def parseGibbsFile(f):
-  dic = {}
-  times = []
-  tuples = []
-  for line in f.readlines():
-    line = line.strip()
-    splits = line.split(" ")
-    if splits[0]=="gibbs":
-      times.append(float(splits[3]))
-    elif splits[0]=="probability":
-      tuples.append(splits[2])
-  for t, time in zip(tuples, times):
-    dic[t] = time
-  return dic
-
-
-
-def parseInfluFile(f):
-  dic = {}
-  times = []
-  tuples = []
+  dic["names"] = []
+  dic["influ"] = []
+  dic["cliques"] = []
+  dic["unobserved"] = []
+  dic["pgibbs"] = []
+  dic["gibbs"] = []
   for line in f.readlines():
     line = line.strip()
     splits = line.split(" ")
     if splits[0]=="mode":
-      times.append(float(splits[6]))
+      dic["influ"].append(float(splits[6]))
     elif splits[0]=="influence":
-      tuples.append(splits[4])
-  for t, time in zip(tuples, times):
-    dic[t] = time
+      dic["names"].append(splits[4])
+    elif splits[0]=="clique":
+      dic["cliques"].append(float(splits[2]))
+    elif splits[0]=="unobserved":
+      dic["unobserved"].append(float(splits[3]))
+    elif splits[0]=="pgibbs":
+      dic["pgibbs"].append(float(splits[3]))
+    elif splits[0]=="gibbs":
+      dic["gibbs"].append(float(splits[3]))
+    elif splits[0]=="probability":
+      dic["names"].append(splits[2])
   return dic
 
 
 
-def selectDict(dic, l):
-  ndic = {}
-  for key in dic:
-    if key in l:
-      ndic[key] = dic[key]
-  return ndic
+# def selectDict(dic, l):
+#   ndic = {}
+#   for key in dic:
+#     if key in l:
+#       ndic[key] = dic[key]
+#   return ndic
 
 
 if __name__=="__main__":
   dire = "sample8"
-  name = "influEquation"
+  name = "influ"
+  queryList = ["cliques", "unobserved", "pgibbs", "gibbs", "influ"]
   file_names = [
             "./data/hypertext-class/"+dire+"/records/"+name+"1.log",
             "./data/hypertext-class/"+dire+"/records/"+name+"2.log",
@@ -75,14 +55,20 @@ if __name__=="__main__":
             "./data/hypertext-class/"+dire+"/records/"+name+"9.log",
             "./data/hypertext-class/"+dire+"/records/"+name+"10.log"
           ]
-  dics = []
+  dics = {}
+  dics["influ"] = []
+  dics["cliques"] = []
+  dics["unobserved"] = []
+  dics["pgibbs"] = []
+  dics["gibbs"] = []
+  dics["prbobabilities"] = []
   avg_times = []
   for file_name in file_names:
     file = open(file_name, "r")
-    # dic = parseGibbsFile(file)
-    # dic = parseFile(file)
-    dic = parseInfluFile(file)
-    dics.append(dic)
-    # dics[-1] = selectDict(dics[-1], list(dics[0].keys()))
-    avg_times.append(np.mean(np.array(list(dics[-1].values()))))
+    dic = parseFileFinal(file)
+    for name in queryList:
+      if len(dic[name])!=0:
+        dics[name].append(sum(dic[name])/len(dic[name]))
+  for name in queryList:
+    print(name, dics[name])
   print(avg_times)
